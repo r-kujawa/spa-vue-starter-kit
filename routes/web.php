4e497d83b526ed3e873d6442790679d9ec50e7ca
Robert Kujawa <robert.kujawa@proton.me>
2025-04-18 11:26:54 -0600

    Remove inertia from the back-end

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-// Route::get('dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
